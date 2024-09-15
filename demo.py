a4d89d8b2af61ad7a23146408f1390f6ad7d187e
Alicia <alicia.spongh@gmail.com>
2024-09-15 11:49:33 +0200

    prompt + testdata

--- demo.py
+++ demo.py
@@ -5,7 +5,10 @@ from typing import Callable, List, Dict
 
 class ChatBot:
     def __init__(self, history: List[Dict[str, str]]):
-        self.model = genai.GenerativeModel('gemini-1.5-flash')
+        self.model = genai.GenerativeModel(
+            model_name = 'gemini-1.5-flash',
+            system_instruction="You are an expert in the machine learning course: advanced probabilistic machine learning. If you are unsure about an answer, say so."
+            )
         self.chat = self.model.start_chat(history=[
             {
                 'role': 'model' if  message['role']=='ai' else message['role'],
@@ -44,6 +47,9 @@ def draw_page(response_generator: Callable[[str], str]) -> None:
     # Write title
     st.header('1RT730 Chatbot')
 
+    st.sidebar.title("Predefinied functions")
+    st.sidebar.button('Generate Exam', generate_exam)
+
     # Display chat history
     for message in st.session_state.messages:
         with st.chat_message(message['role']):
@@ -52,6 +58,7 @@ def draw_page(response_generator: Callable[[str], str]) -> None:
     # Accept user input
     if prompt := st.chat_input('Say something...'):
         with st.chat_message('user'):
+            instruction = "You are an expert in the machine learning course: advanced probabilistic machine learning. If you are unsure about an answer, say so."
             st.markdown(prompt)
         
         st.session_state.messages.append({
@@ -70,6 +77,9 @@ def draw_page(response_generator: Callable[[str], str]) -> None:
             'content': response
         })
 
+def generate_exam(): 
+    pass
+
 def main() -> None:
     """
     Main function. 
@@ -78,10 +88,10 @@ def main() -> None:
     if 'messages' not in st.session_state:
         st.session_state.messages = [{
             'role': 'ai',
-            'content': 'What can I help you with? '
+            'content': "Welcome to the 1RT730 Chatbot. How can I help you today?"
         }]
     
-    # Crate chatbot
+    # Create chatbot
     chat_bot = ChatBot(st.session_state.messages)
 
     # Draw page
